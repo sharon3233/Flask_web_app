a69061a3e3ea6748754d4e680168c29f2751c232
sharon3233 <shalaskin29@gmail.com>
2022-01-27 16:53:59 -0600

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Flask_web_app
+
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running The App
+
+```bash
+python main.py
+```
+
+## Viewing The App
+
+Go to `http://127.0.0.1:5000`
+
+
+# how to write if statements in python
+{% if boolean == True %}
+Yes it is True!
+{% else %}
+No it is not true.
+{% endif %}
+
+{% endblock %}
---
+++ website/views.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, render_template, request, flash, jsonify, Response
+from flask_login import login_required, current_user
+from .models import Note, Img
+from . import db
+import json
+from werkzeug.utils import secure_filename
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+#What I added for my retaurant images Lines 28-53
+@views.route('/upload', methods=['POST'])
+def upload():
+    pic = request.files['pic']
+    if not pic:
+        return 'No pic uploaded!', 400
+    else:
+
+        filename = secure_filename(pic.filename)
+        mimetype = pic.mimetype
+    if not filename or not mimetype:
+        return 'Bad upload!', 400
+
+    img = Img(img=pic.read(), name=filename, mimetype=mimetype)
+    db.session.add(img)
+    db.session.commit()
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/<int:id>')
+def get_img(id):
+    img = Img.query.filter_by(id=id).first()
+    if not img:
+        return 'Img Not Found!', 404
+
+    return Response(img.img, mimetype=img.mimetype)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+
